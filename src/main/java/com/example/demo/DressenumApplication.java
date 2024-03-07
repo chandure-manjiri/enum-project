a29204fec69af55733bf95bae1b381ec27f5d375
manjiri-chandure <chanduremanjiri@gmail.com>
2024-03-07 08:28:55 +0530

    swagger ui added

--- src/main/java/com/example/demo/DressenumApplication.java
+++ src/main/java/com/example/demo/DressenumApplication.java
@@ -1,8 +1,28 @@
 package com.example.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Dress Enum",
+				version = "1.0.0",
+				description = "This project is used to implement dress using enum",
+				termsOfService = "Copyright@2023",
+				contact = @Contact(
+						name = "Manjiri Chandure",
+						email = "chanduremanjiri@gmail.com"
+				),
+				license = @License(
+						name = "license",
+						url = "/dresses"
+				)
+		)
+)
 @SpringBootApplication
 public class DressenumApplication {
 
